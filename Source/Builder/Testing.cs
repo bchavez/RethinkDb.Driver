0652997f94c081deccd25f62d92f8c7658619607
bchavez <bchavez@bitarmory.com>
2015-08-29 10:13:52 -0700

    Fixing build error

---
+++ Source/Builder/Testing.cs
@@ -0,0 +1,24 @@
+using FluentBuild;
+using NUnit.Framework;
+
+namespace Builder
+{
+    [TestFixture]
+    public class Testing
+    {
+        [TestFixtureSetUp]
+        public void BeforeRunningTestSession()
+        {
+            Defaults.Logger.Verbosity = VerbosityLevel.Full;
+
+            System.IO.Directory.SetCurrentDirectory( @"..\..\..\.." );
+        }
+
+        [Test]
+        [Explicit]
+        public void Test()
+        {
+
+        }
+    }
+}

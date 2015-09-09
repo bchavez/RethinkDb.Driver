e110cc4bfeef0b2ab388c0da615804e6e28c63ec
bchavez <bchavez@bitarmory.com>
2015-09-08 23:25:33 -0700

    Nuget push alpha3

--- Source/RethinkDb.Driver/Properties/AssemblyInfo.cs
+++ Source/RethinkDb.Driver/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyrightAttribute("Brian Chavez © 2015")]
 [assembly: AssemblyVersionAttribute("0.0.4")]
 [assembly: AssemblyFileVersion("0.0.4")]
-[assembly: AssemblyInformationalVersion("0.0.4-alpha2 built on 9/7/2015 12:09:54 AM UTC")]
+[assembly: AssemblyInformationalVersion("0.0.4-alpha3 built on 9/9/2015 6:23:46 AM UTC")]
 [assembly: AssemblyTrademark("MIT License")]
 [assembly: AssemblyDescriptionAttribute("http://www.github.com/bchavez/RethinkDb.Driver")]
 [assembly: ComVisible(false)]

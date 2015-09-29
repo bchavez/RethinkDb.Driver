7e3cad4215e014748b0bba068cb47874484b43ea
bchavez <bchavez@bitarmory.com>
2015-09-29 12:55:44 -0700

    Java tests successfully converted to Yaml

--- Source/Templates/GeneratorForUnitTests.cs
+++ Source/Templates/GeneratorForUnitTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -13,34 +15,67 @@ using Z.ExtensionMethods;
 
 namespace Templates
 {
-	[TestFixture]
+    public class YamlTest
+    {
+        public string ModuleName { get; set; }
+        public string[] TableVarNames { get; set; }
+        public DefTest[] DefsAndTests { get; set; }
+
+        [YamlMember(Alias = "render_something")]
+        public bool RenderSomething { get; set; }
+
+        public class DefTest
+        {
+            public string TestType { get; set; }
+            public string TestFile { get; set; }
+            public int TestNum { get; set; }
+            public string Original { get; set; }
+            public string Java { get; set; }
+            public string ExpectedType { get; set; }
+            public string ExpectedJava { get; set; }
+            public string Obtained { get; set; }
+            public List<RunOpt> RunOpts { get; set; }
+            public bool RenderSomething { get; set; }
+        }
+
+        public class RunOpt
+        {
+            public string Key { get; set; }
+            public string Val { get; set; }
+        }
+
+        public void Decode()
+        {
+            //runs decoding on all encoded b64 encoded values
+            //to avoid dealing with yaml ', :, and " edge cases
+            // yaml reparses : after it's been used once
+            //and 
+
+            if( DefsAndTests == null ) return;
+            
+            foreach( var test in DefsAndTests )
+            {
+                var value = test.Original;
+                if( value.IsNotNullOrWhiteSpace() )
+                {
+                    test.Original =
+                        Encoding.UTF8.GetString(Convert.FromBase64String(value.Substring(1).Trim('\'')));
+                }
+
+                value = test.ExpectedJava;
+                if( value.IsNotNullOrWhiteSpace() )
+                {
+                    test.ExpectedJava =
+                        Encoding.UTF8.GetString(Convert.FromBase64String(value.Substring(1).Trim('\'')));
+                }
+            }
+        }
+    }
+
+
+    [TestFixture]
 	public class GeneratorForUnitTests
 	{
-		[TestFixtureSetUp]
-		public void BeforeRunningTestSession()
-		{
-
-		}
-
-		[TestFixtureTearDown]
-		public void AfterRunningTestSession()
-		{
-
-		}
-
-
-		[SetUp]
-		public void BeforeEachTest()
-		{
-
-		}
-
-		[TearDown]
-		public void AfterEachTest()
-		{
-
-		}
-
 		private const string YamlImportDir = "../../UnitTests";
 		private const string JsonOutputDir = "../../UnitTests";
 
@@ -52,24 +87,18 @@ namespace Templates
 	        foreach( var file in files )
 	        {
 	            var lines = File.ReadAllLines(file);
+	            var sb = new StringBuilder();
+	            var sw = new StringWriter(sb);
 	            for( int i = 0; i < lines.Length; i++ )
 	            {
 	                var line = lines[i];
-	                if( line.IndexOf(':') != line.LastIndexOf(':'))
-	                {
-	                    if( line.IndexOf('"') > 0 )
-	                    {
-                            //line has " and : so enclose it with ''
-	                        lines[i] = $"{line.GetBefore(":")}: '{line.GetAfter(":").Trim().Replace("'",@"""")}'";
-	                    }
-                        else if( line.IndexOf('\'') > 0 )
-                        {
-                            // line has ' and : so enclsoe with "
-                            lines[i] = $"{line.GetBefore(":")}: \"{line.GetAfter(":").Trim().Replace(@"""", "'")}\"";
-                        }
-	                }
+                    if( line.StartsWith("//"))
+                        continue;
+
+	                sw.WriteLine(line);
 	            }
-	            File.WriteAllLines(file, lines.Skip(4));
+                
+	            File.WriteAllText(file, sb.ToString());
 	        }
 	    }
 
@@ -87,27 +116,35 @@ namespace Templates
 			    var sr = new StringReader(File.ReadAllText(file));
 
                 var d = new Deserializer();
-				var yobj = d.Deserialize(sr);
+				//var yobj = d.Deserialize(sr);
+			    var yobj = d.Deserialize<YamlTest>(sr);
 
-				var sb = new StringBuilder();
-				var sw = new StringWriter(sb);
-				js.Serialize(sw, yobj);
+			    yobj.Decode();
 
-				var relPath = file;
-
-				var jsonPath = Path.Combine(JsonOutputDir, relPath);
-				var jsonFullPath = Path.GetFullPath(jsonPath);
-				jsonFullPath = Path.ChangeExtension(jsonFullPath, ".json");
-
-				var dirPath = Path.GetDirectoryName(jsonFullPath);
-				if( !Directory.Exists(dirPath) )
-					Directory.CreateDirectory(dirPath);
-
-
-				var json = JObject.Parse(sw.ToString()).ToString(Formatting.Indented);
-				File.WriteAllText(jsonFullPath, json);
+			    OutputToJson(yobj, js, file);
 			}
 		}
+
+        private void OutputToJson(YamlTest yobj, Serializer js, string file)
+        {
+            var sb = new StringBuilder();
+            var sw = new StringWriter(sb);
+            js.Serialize(sw, yobj);
+
+            var relPath = file;
+
+            var jsonPath = Path.Combine(JsonOutputDir, relPath);
+            var jsonFullPath = Path.GetFullPath(jsonPath);
+            jsonFullPath = Path.ChangeExtension(jsonFullPath, ".json");
+
+            var dirPath = Path.GetDirectoryName(jsonFullPath);
+            if (!Directory.Exists(dirPath))
+                Directory.CreateDirectory(dirPath);
+
+
+            var json = JObject.Parse(sw.ToString()).ToString(Formatting.Indented);
+            File.WriteAllText(jsonFullPath, json);
+        }
 
 
 		private string[] GetAllYamlFiles()

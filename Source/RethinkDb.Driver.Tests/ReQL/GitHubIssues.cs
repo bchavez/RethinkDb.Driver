a7c8897b50693ac763d105d97837a5df66d1781c
bchavez <bchavez@bitarmory.com>
2015-11-18 16:59:44 -0800

    Cant reproduce #13. Need more info.

--- Source/RethinkDb.Driver.Tests/ReQL/GitHubIssues.cs
+++ Source/RethinkDb.Driver.Tests/ReQL/GitHubIssues.cs
@@ -1,19 +1,33 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
+using RethinkDb.Driver.Tests.Utils;
 
 namespace RethinkDb.Driver.Tests.ReQL
 {
     [TestFixture]
     [Explicit]
-    public class TestFixture : QueryTestFixture
+    public class GitHubIssues : QueryTestFixture
     {
         [Test]
         public void issue_12()
         {
+            var table = r.db(DbName).table(TableName);
+            table.delete().run(conn);
 
-            r.db(DbName).table(TableName).delete().run(conn);
+            var game = new Game {id = 99, player = "cowboy", points = 5, type = "open"};
 
-            r.db(DbName).table(TableName)
-                .insert(new { Foo = "Bar", id = "foo" });
+            table.insert(game).run(conn);
+
+            game.type = "close";
+
+            table.update(game).run(conn);
+
+            Game fromDb = table.get(99).run<Game>(conn);
+
+            fromDb.Dump();
+
+            fromDb.id.Should().Be(99);
+            fromDb.type.Should().Be("close");
         }
     }
 

47728f1d141117da1a9e95b21dfbcd307b468824
bchavez <bchavez@bitarmory.com>
2015-11-22 19:21:47 -0800

    Remove some debug code.

--- Source/RethinkDb.Driver/Utils/ExtensionsForType.cs
+++ Source/RethinkDb.Driver/Utils/ExtensionsForType.cs
@@ -27,7 +27,7 @@ namespace RethinkDb.Driver.Utils
     {
         public static T RunSync<T>(this Task<T> task)
         {
-            task.Wait(10000);
+            task.Wait();
             return task.Result;
         }
     }

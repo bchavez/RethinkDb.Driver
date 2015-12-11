e1af16137e6097797d65b334a48640acd1419b9d
bchavez <bchavez@bitarmory.com>
2015-12-11 00:35:54 -0800

    namespace clean up

--- Source/RethinkDb.Driver/Net/Clustering/EpsilonGreedy.cs
+++ Source/RethinkDb.Driver/Net/Clustering/EpsilonGreedy.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Security.Policy;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace RethinkDb.Driver.Net.Clustering
 {

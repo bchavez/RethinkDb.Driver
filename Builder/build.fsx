8484c4c2bb6da6869e75f70e19cc480d34049722
bchavez <bchavez@bitarmory.com>
2017-02-19 07:21:35 -0800

    Run tests against RethinkDB v2.3.6 RC1

--- Builder/build.fsx
+++ Builder/build.fsx
@@ -4,7 +4,7 @@
 //#else
 //#endif
 
-let serverDownload = "https://download.rethinkdb.com/windows/rethinkdb-2.3.5.zip"
+let serverDownload = "http://thanos.atnnn.com/downloads/rc/2.3.6/1/rethinkdb.zip"
 
 
 // include Fake lib
